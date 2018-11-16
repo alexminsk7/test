1a22d5f7126b00cc35e8ca3b07d740a4c19d2085
Alex <lisouski87@gmail.com>
2018-11-16 21:14:18 +0300

    Это третий коммит

---
+++ js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// объявление переменной money
+let myMoney = +prompt("Ваш бюджет на месяц ?", '');
+console.log(myMoney);
+
+// объявление переменной time
+let myTime = prompt("Введите дату в формате YYYY-MM-DD", '');
+console.log(myTime);
+
+// создаем объект
+let appData = {
+  moneyData: myMoney,
+  timeData: myTime,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+
+// создаем переменные с вопросами для пользователя
+let question = prompt('Какая статья расходов?');
+appData.expenses[question] = +prompt('Во сколько обойдется?');
+
+let question2 = prompt('Какая статья расходов?');
+appData.expenses[question] = +prompt('Во сколько обойдется?');
+console.log(appData);
+
+alert(myMoney / 30);
